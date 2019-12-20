899591df984ab24b18b7db13e0a6d7f629258b8a
Hyokil KIM <treqh@outlook.com>
2019-12-20 17:57:32 +0100

    [Add] send email

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -11,6 +11,27 @@ app.listen(5000, () => console.log('Server running on port 5000'));
 
 const request = require('request');
 const cheerio = require('cheerio');
+const nodemailer = require('nodemailer');
+
+// Step 1
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'parceltracker007@gmail.com',
+        pass: 'epitechepitech'
+    },
+    tls: {
+        ciphers:'SSLv3'
+    }
+});
+
+// Step 2
+let mailOptions = {
+    from: 'treqh@outlook.com',
+    to: 'hyok1014@hotmail.com',
+    subject: 'Testing',
+    text: 'Hello'
+}
 
 const url = 'http://service.relaiscolis.com/tracking/suivi_colis.aspx?ens_id=53&num_client=RD25189219&num_colis=3476002201&style=RC&passage=1&exp_id=442903126';
 const interval = 5000; // 5 seconds
@@ -29,6 +50,12 @@ setInterval(() => {
                if (steps != i) {
                    console.log('New step!');
                    steps = i;
+                   transporter.sendMail(mailOptions, (error: any, data: any) => {
+                        if (error)
+                            console.log(error)
+                        else
+                            console.log('Email sent');
+                   });
                }
          })
         } 
