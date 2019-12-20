0ca5704f6067f913adc9d7786d7341207d9a940d
Hyokil KIM <treqh@outlook.com>
2019-12-20 17:04:12 +0100

    [Add] interval of 5 seconds

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -12,13 +12,19 @@ app.listen(5000, () => console.log('Server running on port 5000'));
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('http://service.relaiscolis.com/tracking/suivi_colis.aspx?ens_id=53&num_client=RD25189219&num_colis=3476002201&style=RC&passage=1&exp_id=442903126', (error: any, response: any, html: any) => {
-    if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
+const url = 'http://service.relaiscolis.com/tracking/suivi_colis.aspx?ens_id=53&num_client=RD25189219&num_colis=3476002201&style=RC&passage=1&exp_id=442903126';
+const interval = 5000; // 5 seconds
 
-        $('.historique_table_td_normal').each((i: number, element: any) => {
-            const item = $(element).text()
-            console.log(item);
-        })
-    } 
-});
+setInterval(() => {
+    request(url, (error: any, response: any, html: any) => {
+        if (!error && response.statusCode == 200) {
+            const $ = cheerio.load(html);
+
+           $('.historique_table_td_normal').each((i: number, element: any) => {
+               const item = $(element)
+                   .text();
+               console.log(item);
+         })
+        } 
+    });
+}, interval);
