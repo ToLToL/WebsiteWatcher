9aeab25702d832680dca928062029721e33198d4
Hyokil KIM <treqh@outlook.com>
2019-12-23 21:18:42 +0100

    [Add] knows if there's a difference between 2 images

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,45 +1,65 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const nodemailer = require('nodemailer');
-const webshot = require('webshot');
-const fs = require('fs');
-const PNG = require('pngjs').PNG;
-const pixelmatch = require('pixelmatch');
-const express = require('express');
-import { Request, Response } from 'express';
-import { Server } from 'http';
+const request = require("request");
+const cheerio = require("cheerio");
+const nodemailer = require("nodemailer");
+const webshot = require("webshot");
+const fs = require("fs");
+const PNG = require("pngjs").PNG;
+const pixelmatch = require("pixelmatch");
+const express = require("express");
+const bodyParser = require("body-parser");
+import { Request, Response } from "express";
+import { Server } from "http";
 
-require('dotenv/config');
+require("dotenv/config");
 
 const app = express();
 const router = express.Router();
 
-// express
-router.get('/:url1', (req: Request, res: Response) => {
-    res.send(req.params);
+// Middlewares
+app.use(function(req: Request, res: Response, next: any) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
-
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
-app.listen('4000', () => {
-    console.log('Server is running on port 4000');
-})
+// express
+router.get('/', (req: Request, res: Response) => {
+    console.log(req.query.url1);
+  webshot(req.query.url1, "screenshots/file1.png", function(err: any) {
+    if (!err) {
+      console.log("Screenshot saved!");
+      const img1 = PNG.sync.read(fs.readFileSync("screenshots/file1.png"));
+      const img2 = PNG.sync.read(fs.readFileSync("screenshots/file2.png"));
+      const { width, height } = img1;
+    //   const diff = new PNG({ width, height });
+    //   console.log(diff);
+    
+    //   pixelmatch(img1.data, img2.data, diff.data, width, height, {
+    //     threshold: 0.1
+    //   });
+    
+    const diff = pixelmatch(img1.data, img2.data, null, width, height, {
+        threshold: 0.0
+      });
+      //fs.writeFileSync("screenshots/diff.png", PNG.sync.write(diff));
+      res.send(diff == 0);
+    }
+    else
+        console.log(err);
+  });
+});
 
-
-// webshot('facebook.com', 'screenshots/facebookEN.png', function(err: any) {
-//     if (!err) {
-//         console.log("Screenshot saved!");
-//     }
-// });
-
-// const img1 = PNG.sync.read(fs.readFileSync('screenshots/facebookEN.png'));
-// const img2 = PNG.sync.read(fs.readFileSync('screenshots/facebookFR.png'));
-// const {width, height} = img1;
-// const diff = new PNG({width, height});
- 
-// pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.1});
- 
-// fs.writeFileSync('screenshots/diff.png', PNG.sync.write(diff));
+app.listen("4000", () => {
+  console.log("Server is running on port 4000");
+});
 
 // // Step 1
 // let transporter = nodemailer.createTransport({
@@ -86,6 +106,6 @@ app.listen('4000', () => {
 //                    });
 //                }
 //          })
-//         } 
+//         }
 //     });
 // }, interval);
